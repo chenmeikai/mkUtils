24adbc34840a81b0b0b833cdee562cb02a2863d3
chenmeikai <562899487@qq.com>
2018-03-06 10:42:11 +0800

    update

---
+++ src/main/java/com/chenmeikai/utils/UUidUtil.java
@@ -0,0 +1,17 @@
+package com.chenmeikai.utils;
+
+import java.util.UUID;
+
+public class UUidUtil {
+
+	public static String  getUUID(){
+			UUID uuid=UUID.randomUUID();
+			String  str=uuid.toString();
+			String  uuidStr=str.replaceAll("-", "");
+		return uuidStr;
+	}
+	
+	public static void main(String[] arg){
+		System.out.println( getUUID());		
+	}
+}

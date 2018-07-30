aba35e5f0d4f3175016508d0b6d3c25510e49c63
chenmeikai <562899487@qq.com>
2018-07-30 23:34:06 +0800

    no message

---
+++ src/main/java/com/chenmeikai/utils/HtmlUtils.java
@@ -0,0 +1,41 @@
+package com.chenmeikai.utils;
+
+/**
+ * html工具
+ * @author meikai
+ */
+public class HtmlUtils {
+	
+	public static String htmlEncode(String source) {
+        if (source == null) {
+            return "";
+        }
+        String html = "";
+        StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < source.length(); i++) {
+            char c = source.charAt(i);
+            switch (c) {
+            case '<':
+                buffer.append("&lt;");
+                break;
+            case '>':
+                buffer.append("&gt;");
+                break;
+            case '&':
+                buffer.append("&amp;");
+                break;
+            case '"':
+                buffer.append("&quot;");
+                break;
+            case 10:
+            case 13:
+                break;
+            default:
+                buffer.append(c);
+            }
+        }
+        html = buffer.toString();
+        return html;
+    }
+
+}
